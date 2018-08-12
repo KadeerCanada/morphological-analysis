173fff7c4af63183f0bd94c34b99e6ff208d42c7
assly <blue20will@gmail.com>
2018-08-12 13:13:50 +0900

    add: Create analyser with markov

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 const kuromoji = require('kuromoji');
+const fs = require('fs');
+
+class Markov {
+  constructor() {
+    this.data = {};
+  }
+
+  add(words) {
+    for (let i = 0; i <= words.length; i++) {
+      let now = words[i];
+      if (now === undefined) { now = null; }
+      let prev = words[i - 1];
+      if (prev === undefined) { prev = null; }
+
+      if (this.data[prev] === undefined) {
+        this.data[prev] = [];
+      }
+      this.data[prev].push(now);
+    }
+  }
+
+  sample(word) {
+    let words = this.data[word];
+    if (words === undefined) { words = []; }
+
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
+  make() {
+    const sentence = [];
+    let word = this.sample(null);
+    while (word) {
+      sentence.push(word);
+      word = this.sample(word);
+    }
+
+    return sentence.join('');
+  }
+}
+
+const markov = new Markov();
 
 kuromoji
   .builder({ dicPath: 'node_modules/kuromoji/dict' })
   .build((err, tokenizer) => {
     if (err) { throw err; }
-    const path = tokenizer.tokenize('今日はいい天気ですね');
-    console.log(path);
+
+    fs.readFile('ashitaka.txt', 'utf8', (_err, data) => {
+      const lines = data.split('\n');
+      lines.forEach((line) => {
+        const tokens = tokenizer.tokenize(line);
+        const words = tokens.map(token => token.surface_form);
+        markov.add(words);
+      });
+
+      for (let i = 0; i < 10; i++) {
+        console.log(markov.make());
+      }
+    });
   });
